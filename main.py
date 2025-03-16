606f51b053554a1adf9bb691451e5a2df9f10bdc
William Madgin <127136180+B34st222@users.noreply.github.com>
2025-03-16 00:32:04 -0400

    create api

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}pokemon/{name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Pokemon name: {data['name']}")
+        print(f"Pokemon id: {data['id']}")
+        print(f"Pokemon height: {data['height']}")
+        print(f"Pokemon weight: {data['weight']}")
+        print(f"Pokemon abilities: {data['abilities']}")
+    else:
+        print("Pokemon not found")
+
+
+pokemon_name = "ratata"
+get_pokemon_info(pokemon_name)
